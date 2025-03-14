e7581043f867e94522820c7d4c51af5db3db06ec
BRUNO UCHOA <138736517+DevBrunoUchoa@users.noreply.github.com>
2025-03-14 10:36:14 -0300

    Update gerar_graficos.py

--- python-analise/graficos/gerar_graficos.py
+++ python-analise/graficos/gerar_graficos.py
@@ -3,6 +3,19 @@ from src.graficos import plotar_graficos
 import os
 
 def gerar_e_salvar_graficos():
+    """
+    Gera e salva gráficos comparando os algoritmos FIFO e LRU para diferentes tamanhos de cache.
+
+    Esta função realiza os seguintes passos:
+    1. Define os parâmetros para a simulação:
+       - Número de páginas a serem geradas.
+       - Intervalo de valores possíveis para as páginas.
+       - Número de repetições para cada tamanho de cache.
+       - Tamanhos de cache a serem testados.
+    2. Executa os testes usando a função `executar_teste` para obter os resultados dos algoritmos FIFO e LRU.
+    3. Cria uma pasta chamada "graficos" (se não existir) para armazenar os gráficos gerados.
+    4. Salva o gráfico comparativo na pasta "graficos" com o nome "resultados.png".
+    """
     quantidade_paginas = 10000  
     intervalo_paginas = 5000
     repeticoes = 10
