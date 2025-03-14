fc325d2f41872027340b03975abc49d54e381408
BRUNO UCHOA <138736517+DevBrunoUchoa@users.noreply.github.com>
2025-03-14 10:27:48 -0300

    Update graficos.py

--- python-analise/src/graficos.py
+++ python-analise/src/graficos.py
@@ -1,6 +1,19 @@
 import matplotlib.pyplot as plt
 
 def plotar_graficos(tamanhos_cache, resultados_fifo, resultados_lru, salvar=False, nome_arquivo="resultados.png"):
+    """
+    Plota gráficos comparando as métricas de desempenho dos algoritmos FIFO e LRU.
+
+    Parâmetros:
+    - tamanhos_cache (list): Lista de tamanhos de cache usados nos testes.
+    - resultados_fifo (list): Lista de resultados médios do algoritmo FIFO para cada tamanho de cache.
+    - resultados_lru (list): Lista de resultados médios do algoritmo LRU para cada tamanho de cache.
+    - salvar (bool, opcional): Se True, salva o gráfico em um arquivo. Se False, exibe o gráfico na tela. Padrão é False.
+    - nome_arquivo (str, opcional): Nome do arquivo para salvar o gráfico. Padrão é "resultados.png".
+
+    Retorna:
+    - None
+    """
     metricas = ["Taxa de Acertos", "Taxa de Falhas", "Eficiência Relativa Sobre o Tamanho do Cache", "Frequência de Substituições", "Custo de Substituições", "Tempo Total de Execução"]
     fig, axs = plt.subplots(2, 3, figsize=(18, 10))
     axs = axs.flatten()
